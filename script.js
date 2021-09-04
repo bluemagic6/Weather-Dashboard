f3ba245bdf0afc2f1cc169f10254017e41ebc17c
Johnny Dickerson Jr <lilman.jdick@gmail.com>
2021-09-04 00:11:18 -0500

    added variables

--- script.js
+++ script.js
@@ -4,22 +4,23 @@ var city = document.querySelector('.city')
 var desc = document.querySelector('.desc')
 var temp = document.querySelector('.temp')
 var APIKey = "c8f8c0dc99abc022e07170e47874a788";
-var cityName = document.querySelector('.inputValue');
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
+var cityName = document.querySelector('.inputValue').value;
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + "cityName" + "&appid=" + APIKey;
 
 button.addEventListener('click',function(){
+    var cityName = document.querySelector('.inputValue').value;
     fetch(queryURL)
     .then(response => response.json())
     .then(data => {
         var cityValue = data['city']
         var tempValue = data['main']['temp']
         var deseValue = data['weather'][0]['description']
-
+        
         city.innerHTML = cityValue;
         temp.innerHTML = tempValue;
         desc.innerHTML = deseValue;
     })
-
-
-.catch(err => alert("Wrong City city!"))
+    
+    
+    .catch(err => alert("Wrong City city!"))
 })
