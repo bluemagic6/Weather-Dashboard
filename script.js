275aff31fbaba9f141baee12f26b840c3219f0c6
Johnny Dickerson Jr <lilman.jdick@gmail.com>
2021-09-29 22:34:11 -0500

    added for the five day forecast display

--- script.js
+++ script.js
@@ -4,23 +4,48 @@ var city = document.querySelector('.city')
 var desc = document.querySelector('.desc')
 var temp = document.querySelector('.temp')
 var APIKey = "c8f8c0dc99abc022e07170e47874a788";
-var cityName = document.querySelector('.inputValue').value;
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + "cityName" + "&appid=" + APIKey;
+var iconEl = document.getElementById('icon');
+var fiveDayEl = document.getElementById('fiveDay')
 
 button.addEventListener('click',function(){
     var cityName = document.querySelector('.inputValue').value;
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey + "&units=imperial";
+    // var cityName = document.querySelector('.inputValue').value;
     fetch(queryURL)
     .then(response => response.json())
     .then(data => {
-        var cityValue = data['city']
-        var tempValue = data['main']['temp']
-        var deseValue = data['weather'][0]['description']
+        var cityValue = data.name
+        var tempValue = data.main.temp
+        var deseValue = data.weather[0].description
+        var apiIcon = "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png"
         
         city.innerHTML = cityValue;
+        iconEl.setAttribute("src", apiIcon)
         temp.innerHTML = tempValue;
         desc.innerHTML = deseValue;
+        console.log(cityValue)
     })
     
-    
     .catch(err => alert("Wrong City city!"))
-})
+});
+
+renderFive (city) {
+    const url = `http://api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}&units=imperial`
+
+    fetch(url)
+
+    // for loop
+    for (let i = 1; i < 6; i++){
+        const cityVal = data.city.name
+        const dateVal = data.list[i].dt
+        const tempVal = data.list[i].main.temp
+        const humidVal = data.list[i].main.humidity
+
+
+        const dailyCard = `
+        <h5 class = "card-title">${cityVal}</h5>
+        <p class="card-text">${tempVal}</p>
+        <p class="card-text">${humidVal}</p>
+        `
+    }
+};
